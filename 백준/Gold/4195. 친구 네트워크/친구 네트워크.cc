9066c0989f334e59e5073098d9eea2443b272f07
EGU1832 <egu1832@sogang.ac.kr>
2025-08-13 23:11:24 +0900

    [Gold II] Title: 친구 네트워크, Time: 112 ms, Memory: 18788 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.cc
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.cc
@@ -1,72 +1,65 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
-int find_parent(int* friend_set, int element);
-void union_set(int* friend_set, int* number_set, int element1, int element2);
+int parent(int* fset, int e) {
+	if (fset[e] != e) {
+		fset[e] = parent(fset, fset[e]);
+	}
+	return fset[e];
+}
 
-int main(void)
-{
+void union_set(int* fset, int* nset, int a, int b) {
+	int a_parent = parent(fset, a);
+	int b_parent = parent(fset, b);
+
+	if (a_parent == b_parent) return;
+
+	if (a_parent < b_parent) {
+		fset[b_parent] = a_parent;
+		nset[a_parent] += nset[b_parent];
+	} else {
+		fset[a_parent] = b_parent;
+		nset[b_parent] += nset[a_parent];
+	}
+}
+
+int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int test_case;
+	int T;
+	cin >> T;
 
-	cin >> test_case;
-	for(int i = 0; i < test_case; i++) {
-		int friend_set[200001];
-		int number_of_friends_of[200001];
-		map<string, int> friends_id;
-		int number_of_relationships, id = 0;
+	while (T--) {
+		int fset[200001];
+		int fnum[200001];
+		unordered_map<string, int> fid;
+		int rnum, id = 0;
 
-		for(int j = 0; j < 200001; j++) {
-			friend_set[j] = j;
-			number_of_friends_of[j] = 1;
+		for (int j = 0; j < 200001; j++) {
+			fset[j] = j;
+			fnum[j] = 1;
 		}
 
-		cin >> number_of_relationships;
-		for(int k = 0; k < number_of_relationships; k++) {
-			string friend1, friend2;
-			cin >> friend1 >> friend2;
-			
-			auto it1 = friends_id.find(friend1);
-			auto it2 = friends_id.find(friend2);
-			if (it1 == friends_id.end()) {
-				friends_id[friend1] = ++id;
-			}
-			if (it2 == friends_id.end()) {
-				friends_id[friend2] = ++id;
-			}
-			union_set(friend_set, number_of_friends_of, friends_id[friend1], friends_id[friend2]);
-			cout << number_of_friends_of[find_parent(friend_set, friends_id[friend1])] << "\n";
+		cin >> rnum;
+		for (int k = 0; k < rnum; k++) {
+			string a, b;
+			cin >> a >> b;
+
+			if (fid.find(a) == fid.end()) fid[a] = ++id;
+			if (fid.find(b) == fid.end()) fid[b] = ++id;
+
+			int rootA = parent(fset, fid[a]);
+			int rootB = parent(fset, fid[b]);
+
+			union_set(fset, fnum, rootA, rootB);
+			cout << fnum[parent(fset, rootA)] << "\n";
 		}
 	}
 
 	return 0;
-}
-
-int find_parent(int* friend_set, int element) {
-	if(friend_set[element] == element) {
-		return element;
-	}
-	return find_parent(friend_set, friend_set[element]);
-}
-
-void union_set(int* friend_set, int* number_set, int element1, int element2) {
-	int element1_parent = find_parent(friend_set, element1);
-	int element2_parent = find_parent(friend_set, element2);
-
-	if(element1_parent < element2_parent) {
-		friend_set[element2_parent] = element1_parent;
-		number_set[element1_parent] += number_set[element2_parent];
-	}
-	else if(element1_parent > element2_parent) {
-		friend_set[element1_parent] = element2_parent;
-		number_set[element2_parent] += number_set[element1_parent];
-	}
-
-	return;
 }
